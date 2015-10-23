5f005fc516a771d5c2993b2b331b630e86fdef2b
Bryan Drewery <bdrewery@FreeBSD.org>
2015-10-23 11:21:26 -0700

    Rework subdir.mk for make(target)
    This removes the need for the buildincludes/installincludes splitting
    from r289462.  Revert to just 1 tree walk of 'includes' now.

--- share/mk/bsd.subdir.mk
+++ share/mk/bsd.subdir.mk
@@ -36,10 +36,11 @@
 .if !target(__<bsd.subdir.mk>__)
 __<bsd.subdir.mk>__:
 
-ALL_SUBDIR_TARGETS= all all-man buildconfig checkdpadd clean cleandepend \
-		    cleandir cleanilinks cleanobj depend distribute \
-		    installconfig lint maninstall manlint obj objlink \
-		    realinstall regress tags \
+ALL_SUBDIR_TARGETS= all all-man buildconfig buildfiles buildincludes \
+		    checkdpadd clean cleandepend cleandir cleanilinks \
+		    cleanobj depend distribute files includes installconfig \
+		    installfiles installincludes install lint maninstall \
+		    manlint obj objlink regress tags \
 		    ${SUBDIR_TARGETS}
 
 # Described above.
@@ -62,6 +63,7 @@ SUBDIR:=${SUBDIR} ${SUBDIR.yes}
 SUBDIR:=${SUBDIR:u}
 .endif
 
+# XXX: Likely can move some of this to bsd.sys.mk now.
 DISTRIBUTION?=	base
 .if !target(distribute)
 distribute: .MAKE
@@ -69,6 +71,28 @@ distribute: .MAKE
 	${_+_}cd ${.CURDIR}; \
 	    ${MAKE} install -DNO_SUBDIR DESTDIR=${DISTDIR}/${dist} SHARED=copies
 .endfor
+.endif
+
+# Convenience targets to run 'build${target}' and 'install${target}' when
+# calling 'make ${target}'.
+# XXX: Move to incs/confs/files
+.for __target in files includes
+.if !target(${__target})
+${__target}:	build${__target} install${__target}
+.ORDER:		build${__target} install${__target}
+.endif
+.endfor
+
+# Make 'install' supports a before and after target.  Actual install
+# hooks are placed in 'realinstall'.
+.if !target(install)
+.for __stage in before real after
+.if !target(${__stage}install)
+${__stage}install:
+.endif
+.endfor
+install:	beforeinstall realinstall afterinstall
+.ORDER:		beforeinstall realinstall afterinstall
 .endif
 
 # Subdir code shared among 'make <subdir>', 'make <target>' and SUBDIR_PARALLEL.
@@ -82,7 +106,7 @@ _SUBDIR_SH=	\
 
 _SUBDIR: .USEBEFORE
 .if defined(SUBDIR) && !empty(SUBDIR) && !defined(NO_SUBDIR)
-	@${_+_}target=${.TARGET:S,realinstall,install,}; \
+	@${_+_}target=${.TARGET}; \
 	    for dir in ${SUBDIR:N.WAIT}; do ( ${_SUBDIR_SH} ); done
 .endif
 
@@ -101,6 +125,11 @@ SUBDIR:=	${SUBDIR:N.WAIT}
 .else
 _is_standalone_target=	0
 .endif
+# Only recurse on directly-called targets.  I.e., don't recurse on dependencies
+# such as 'install' becoming {before,real,after}install, just recurse
+# 'install'.
+.if make(${__target}) || make(par-${__target})
+par-${__target}: .PHONY ${__target}
 .if defined(SUBDIR_PARALLEL) || ${_is_standalone_target} == 1
 __subdir_targets=
 .for __dir in ${SUBDIR}
@@ -116,7 +145,7 @@ __deps+= ${__target}_subdir_${__dep}
 .endif
 ${__target}_subdir_${__dir}: .PHONY .MAKE ${__deps}
 .if !defined(NO_SUBDIR)
-	@${_+_}target=${__target:realinstall=install}; \
+	@${_+_}target=${__target}; \
 	    dir=${__dir}; \
 	    ${_SUBDIR_SH};
 .endif
@@ -126,35 +155,11 @@ ${__target}: ${__subdir_targets}
 .else
 ${__target}: _SUBDIR
 .endif	# SUBDIR_PARALLEL || _is_standalone_target
+.elif !target(${__target})
+${__target}:
+.endif	# make(${__target})
 .endfor	# __target in ${ALL_SUBDIR_TARGETS}
 
-# This is to support 'make includes' calling 'make buildincludes' and
-# 'make installincludes' in the proper order, and to support these
-# targets as SUBDIR_TARGETS.
-.for __target in files includes
-.for __stage in build install
-${__stage}${__target}:
-.if make(${__stage}${__target})
-${__stage}${__target}: _SUBDIR
-.endif
-.endfor
-.if !target(${__target})
-${__target}: .MAKE
-	${_+_}cd ${.CURDIR}; ${MAKE} build${__target}; ${MAKE} install${__target}
-.endif
-.endfor
-
-.endif
-
-.if !target(install)
-.if !target(beforeinstall)
-beforeinstall:
-.endif
-.if !target(afterinstall)
-afterinstall:
-.endif
-install: beforeinstall realinstall afterinstall
-.ORDER: beforeinstall realinstall afterinstall
-.endif
+.endif	# !target(_SUBDIR)
 
 .endif
