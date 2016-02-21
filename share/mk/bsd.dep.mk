64889f50a95a19d776b481ee44bc72e294761e57
Bryan Drewery <bdrewery@FreeBSD.org>
2016-02-20 16:26:41 -0800

    Blah depend on headers only...

--- share/mk/bsd.dep.mk
+++ share/mk/bsd.dep.mk
@@ -226,7 +226,11 @@ ${__obj}: ${OBJS_DEPEND_GUESS.${__obj}}
 # need for manually running it.  The dirdeps build should only do this in
 # sub-makes though since MAKELEVEL0 is for dirdeps calculations.
 .if ${MK_DIRDEPS_BUILD} == "no" || ${.MAKE.LEVEL} > 0
-beforebuild: depend
+#beforebuild: ${SRCS:M*.h} ${DPSRCS}
+# Ensure we generate if needed for _EXTRADEPEND
+.if commands(beforedepend) || commands(afterdepend) || target(_EXTRADEPEND)
+all: depend
+.endif
 .endif
 .endif
 
