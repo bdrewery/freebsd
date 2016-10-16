bdrewery/freebsd

commit 80ee958509e991d5bd869c0d0e8275b34f3d43af

Staticfy and constify some variables and clean up the code a bit to make it more readable. No functional change.
Differential Revision:	D3166
Reviewed by:		kib
Sponsored by:		gandi.net


git-svn-id: svn+ssh://svn.freebsd.org/base/head@285926 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

libexec/ypxfr/ypxfr_getmap.c
@@ -43,8 +43,8 @@ __FBSDID("$FreeBSD$");
 
 extern bool_t xdr_ypresp_all_seq(XDR *, unsigned long *);
 
-int (*ypresp_allfn)();
-void *ypresp_data;
+static int (*ypresp_allfn)();
+static void *ypresp_data;
 extern DB *specdbp;
 extern enum ypstat yp_errno;
 

libexec/ypxfr/ypxfr_main.c
@@ -54,12 +54,12 @@ __FBSDID("$FreeBSD$");
 char *progname = "ypxfr";
 char *yp_dir = _PATH_YP;
 int _rpcpmstart = 0;
-int ypxfr_use_yplib = 0; /* Assume the worst. */
-int ypxfr_clear = 1;
-int ypxfr_prognum = 0;
-struct sockaddr_in ypxfr_callback_addr;
-struct yppushresp_xfr ypxfr_resp;
-DB *dbp;
+static int ypxfr_use_yplib = 0; /* Assume the worst. */
+static int ypxfr_clear = 1;
+static int ypxfr_prognum = 0;
+static struct sockaddr_in ypxfr_callback_addr;
+static struct yppushresp_xfr ypxfr_resp;
+static DB *dbp;
 
 static void
 ypxfr_exit(ypxfrstat retval, char *temp)

libexec/ypxfr/ypxfrd_getmap.c
@@ -47,7 +47,7 @@ __FBSDID("$FreeBSD$");
 #include <sys/types.h>
 #include "ypxfr_extern.h"
 
-int fp = 0;
+static int fp = 0;
 
 static bool_t
 xdr_my_xfr(register XDR *xdrs, xfr *objp)

usr.bin/ypcat/ypcat.c
@@ -47,9 +47,9 @@ __FBSDID("$FreeBSD$");
 #include <string.h>
 #include <unistd.h>
 
-struct ypalias {
+const struct ypalias {
 	char *alias, *name;
-} ypaliases[] = {
+} static ypaliases[] = {
 	{ "passwd", "passwd.byname" },
 	{ "master.passwd", "master.passwd.byname" },
 	{ "shadow", "shadow.byname" },
@@ -62,7 +62,7 @@ struct ypalias {
 	{ "ethers", "ethers.byname" },
 };
 
-int key;
+static int key;
 
 static void
 usage(void)

usr.bin/ypmatch/ypmatch.c
@@ -47,9 +47,9 @@ __FBSDID("$FreeBSD$");
 #include <string.h>
 #include <unistd.h>
 
-struct ypalias {
+const struct ypalias {
 	char *alias, *name;
-} ypaliases[] = {
+} static ypaliases[] = {
 	{ "passwd", "passwd.byname" },
 	{ "master.passwd", "master.passwd.byname" },
 	{ "shadow", "shadow.byname" },

usr.bin/ypwhich/ypwhich.c
@@ -59,9 +59,9 @@ __FBSDID("$FreeBSD$");
 
 extern bool_t xdr_domainname();
 
-struct ypalias {
+const struct ypalias {
 	char *alias, *name;
-} ypaliases[] = {
+} static ypaliases[] = {
 	{ "passwd", "passwd.byname" },
 	{ "master.passwd", "master.passwd.byname" },
 	{ "shadow", "shadow.byname" },

usr.sbin/yp_mkdb/yp_mkdb.c
@@ -51,7 +51,6 @@ __FBSDID("$FreeBSD$");
 #include "ypxfr_extern.h"
 
 char *yp_dir = "";	/* No particular default needed. */
-int _rpcpmstart = 0;
 int debug = 1;
 
 static void
@@ -66,7 +65,6 @@ usage(void)
 }
 
 #define PERM_SECURE (S_IRUSR|S_IWUSR)
-
 static DB *
 open_db(char *path, int flags)
 {
@@ -185,7 +183,6 @@ main(int argc, char *argv[])
 	 * write to stdout; the db library doesn't let you
 	 * write to a file stream like that.
 	 */
-
 	if (!strcmp(infile, "-")) {
 		ifp = stdin;
 	} else {
@@ -327,7 +324,6 @@ main(int argc, char *argv[])
 	(void)(dbp->close)(dbp);
 
 doclear:
-
 	if (clear) {
 		char in = 0;
 		char *out = NULL;

usr.sbin/yppush/yppush_main.c
@@ -58,15 +58,15 @@ int debug = 1;
 int _rpcpmstart = 0;
 char *yp_dir = _PATH_YP;
 
-char *yppush_mapname = NULL;	/* Map to transfer. */
-char *yppush_domain = NULL;	/* Domain in which map resides. */
-char *yppush_master = NULL;	/* Master NIS server for said domain. */
-int skip_master = 0;		/* Do not attempt to push map to master. */
-int verbose = 0;		/* Toggle verbose mode. */
-unsigned long yppush_transid = 0;
-int yppush_timeout = 80;	/* Default timeout. */
-int yppush_jobs = 1;		/* Number of allowed concurrent jobs. */
-int yppush_running_jobs = 0;	/* Number of currently running jobs. */
+static char *yppush_mapname = NULL;	/* Map to transfer. */
+static char *yppush_domain = NULL;	/* Domain in which map resides. */
+static char *yppush_master = NULL;	/* Master NIS server for said domain. */
+static int skip_master = 0;		/* Do not attempt to push map to master. */
+static int verbose = 0;		/* Toggle verbose mode. */
+static unsigned long yppush_transid = 0;
+static int yppush_timeout = 80;	/* Default timeout. */
+static int yppush_jobs = 1;		/* Number of allowed concurrent jobs. */
+static int yppush_running_jobs = 0;	/* Number of currently running jobs. */
 
 /* Structure for holding information about a running job. */
 struct jobs {
@@ -80,8 +80,7 @@ struct jobs {
 	struct jobs *next;
 };
 
-struct jobs *yppush_joblist;	/* Linked list of running jobs. */
-
+static struct jobs *yppush_joblist;	/* Linked list of running jobs. */
 static int yppush_svc_run(int);
 
 /*
@@ -464,7 +463,8 @@ yppush_foreach(int status, char *key, int keylen, char *val, int vallen,
 	return (0);
 }
 
-static void usage()
+static void
+usage()
 {
 	fprintf (stderr, "%s\n%s\n",
 	"usage: yppush [-d domain] [-t timeout] [-j #parallel jobs] [-h host]",

commit 11baa16abf381c95093c4d7888adbd53f80de195

Reference the libc symbols ypresp_{allfn,data} instead of local symbols.
This fixes a regression introduced in r285926.

PR:		213506
MFC after:	3 days


git-svn-id: svn+ssh://svn.freebsd.org/base/head@307398 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

libexec/ypxfr/ypxfr_getmap.c
@@ -43,8 +43,8 @@ __FBSDID("$FreeBSD$");
 
 extern bool_t xdr_ypresp_all_seq(XDR *, unsigned long *);
 
-static int (*ypresp_allfn)();
-static void *ypresp_data;
+extern int (*ypresp_allfn)();
+extern void *ypresp_data;
 extern DB *specdbp;
 extern enum ypstat yp_errno;
 