bdrewery/freebsd

commit e1af6c38d3f500a48793f58360c75814b7453861

Remove pc98 support completely. I thank all developers and contributors for pc98.
Relnotes:	yes


git-svn-id: svn+ssh://svn.freebsd.org/base/head@312910 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/x86/x86/cpu_machdep.c
@@ -243,7 +243,6 @@ static int	idle_mwait = 1;		/* Use MONITOR/MWAIT for short idle. */
 SYSCTL_INT(_machdep, OID_AUTO, idle_mwait, CTLFLAG_RWTUN, &idle_mwait,
     0, "Use MONITOR/MWAIT for short idle");
 
-#ifndef PC98
 static void
 cpu_idle_acpi(sbintime_t sbt)
 {
@@ -262,7 +261,6 @@ cpu_idle_acpi(sbintime_t sbt)
 		acpi_cpu_c1();
 	*state = STATE_RUNNING;
 }
-#endif /* !PC98 */
 
 static void
 cpu_idle_hlt(sbintime_t sbt)
@@ -369,11 +367,7 @@ cpu_probe_amdc1e(void)
 	}
 }
 
-#if defined(__i386__) && defined(PC98)
-void (*cpu_idle_fn)(sbintime_t) = cpu_idle_hlt;
-#else
 void (*cpu_idle_fn)(sbintime_t) = cpu_idle_acpi;
-#endif
 
 void
 cpu_idle(int busy)
@@ -450,7 +444,7 @@ struct {
 	{ cpu_idle_spin, "spin" },
 	{ cpu_idle_mwait, "mwait" },
 	{ cpu_idle_hlt, "hlt" },
-#if !defined(__i386__) || !defined(PC98)
+#if !defined(__i386__)
 	{ cpu_idle_acpi, "acpi" },
 #endif
 	{ NULL, NULL }
@@ -469,7 +463,7 @@ idle_sysctl_available(SYSCTL_HANDLER_ARGS)
 		if (strstr(idle_tbl[i].id_name, "mwait") &&
 		    (cpu_feature2 & CPUID2_MON) == 0)
 			continue;
-#if !defined(__i386__) || !defined(PC98)
+#if !defined(__i386__)
 		if (strcmp(idle_tbl[i].id_name, "acpi") == 0 &&
 		    cpu_idle_hook == NULL)
 			continue;
@@ -508,7 +502,7 @@ idle_sysctl(SYSCTL_HANDLER_ARGS)
 		if (strstr(idle_tbl[i].id_name, "mwait") &&
 		    (cpu_feature2 & CPUID2_MON) == 0)
 			continue;
-#if !defined(__i386__) || !defined(PC98)
+#if !defined(__i386__)
 		if (strcmp(idle_tbl[i].id_name, "acpi") == 0 &&
 		    cpu_idle_hook == NULL)
 			continue;

commit 092c2eeecdce5738eb845f384ae3b0434670dd5f

Fix the acpi idle support on i386 which was broken by r312910.
The ifdefs were '#if !defined(__i386__) || !defined(PC98)' previously,
so cpu_idle_acpi was enabled both i386 and amd64 except PC98.

I was obfuscated by '#if !defined(__i386__)' condition.

Submitted by:	bde
Reported by:	bde


git-svn-id: svn+ssh://svn.freebsd.org/base/head@314301 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/x86/x86/cpu_machdep.c
@@ -451,9 +451,7 @@ struct {
 	{ cpu_idle_spin, "spin" },
 	{ cpu_idle_mwait, "mwait" },
 	{ cpu_idle_hlt, "hlt" },
-#if !defined(__i386__)
 	{ cpu_idle_acpi, "acpi" },
-#endif
 	{ NULL, NULL }
 };
 
@@ -470,11 +468,9 @@ idle_sysctl_available(SYSCTL_HANDLER_ARGS)
 		if (strstr(idle_tbl[i].id_name, "mwait") &&
 		    (cpu_feature2 & CPUID2_MON) == 0)
 			continue;
-#if !defined(__i386__)
 		if (strcmp(idle_tbl[i].id_name, "acpi") == 0 &&
 		    cpu_idle_hook == NULL)
 			continue;
-#endif
 		p += sprintf(p, "%s%s", p != avail ? ", " : "",
 		    idle_tbl[i].id_name);
 	}
@@ -509,11 +505,9 @@ idle_sysctl(SYSCTL_HANDLER_ARGS)
 		if (strstr(idle_tbl[i].id_name, "mwait") &&
 		    (cpu_feature2 & CPUID2_MON) == 0)
 			continue;
-#if !defined(__i386__)
 		if (strcmp(idle_tbl[i].id_name, "acpi") == 0 &&
 		    cpu_idle_hook == NULL)
 			continue;
-#endif
 		if (strcmp(idle_tbl[i].id_name, buf))
 			continue;
 		cpu_idle_fn = idle_tbl[i].id_fn;