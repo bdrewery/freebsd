bdrewery/freebsd

commit 82b4fe5a9355d08633ae38b7d0db145b3e37f8db

PowerNV: move LPCR and LPID altering to cpudep_ap_early_bootstrap
It turns out that under some circumstances we can get DSI or DSE before we set
LPCR and LPID so we should set it as early as possible.

Authored by:           Patryk Duda <pdk@semihalf.com>
Submitted by:          Wojciech Macek <wma@semihalf.com>
Obtained from:         Semihalf
Sponsored by:          IBM, QCM Technologies


git-svn-id: svn+ssh://svn.freebsd.org/base/head@328537 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/powerpc/aim/mp_cpudep.c
@@ -64,9 +64,6 @@ cpudep_ap_early_bootstrap(void)
 	register_t reg;
 #endif
 
-	__asm __volatile("mtsprg 0, %0" :: "r"(ap_pcpu));
-	powerpc_sync();
-
 	switch (mfpvr() >> 16) {
 	case IBM970:
 	case IBM970FX:
@@ -86,7 +83,20 @@ cpudep_ap_early_bootstrap(void)
 #endif
 		powerpc_sync();
 		break;
+	case IBMPOWER8:
+	case IBMPOWER8E:
+		isync();
+		/* Direct interrupts to SRR instead of HSRR and reset LPCR otherwise */
+		mtspr(SPR_LPID, 0);
+		isync();
+
+		mtspr(SPR_LPCR, LPCR_LPES);
+		isync();
+		break;
 	}
+
+	__asm __volatile("mtsprg 0, %0" :: "r"(ap_pcpu));
+	powerpc_sync();
 }
 
 uintptr_t

sys/powerpc/powernv/platform_powernv.c
@@ -128,6 +128,7 @@ powernv_attach(platform_t plat)
 	pcell_t prop;
 	phandle_t cpu;
 	int res, len, node, idx;
+	register_t msr;
 
 	/* Ping OPAL again just to make sure */
 	opal_check();
@@ -140,6 +141,19 @@ powernv_attach(platform_t plat)
 
 	cpu_idle_hook = powernv_cpu_idle;
 	powernv_boot_pir = mfspr(SPR_PIR);
+
+	/* LPID must not be altered when PSL_DR or PSL_IR is set */
+	msr = mfmsr();
+	mtmsr(msr & ~(PSL_DR | PSL_IR));
+
+	/* Direct interrupts to SRR instead of HSRR and reset LPCR otherwise */
+	mtspr(SPR_LPID, 0);
+	isync();
+
+	mtmsr(msr);
+
+	mtspr(SPR_LPCR, LPCR_LPES);
+	isync();
 
 	/* Init CPU bits */
 	powernv_smp_ap_init(plat);
@@ -444,21 +458,6 @@ powernv_reset(platform_t platform)
 static void
 powernv_smp_ap_init(platform_t platform)
 {
-	register_t msr;
-
-	/* LPID must not be altered when PSL_DR or PSL_IR is set */
-	msr = mfmsr();
-	mtmsr(msr & ~(PSL_DR | PSL_IR));
-
-	isync();
-	/* Direct interrupts to SRR instead of HSRR and reset LPCR otherwise */
-	mtspr(SPR_LPID, 0);
-	isync();
-
-	mtmsr(msr);
-
-	mtspr(SPR_LPCR, LPCR_LPES);
-	isync();
 }
 
 static void

commit 09578696e84dc9a23bc3480fa8fd656c35c212a1

Fix build on 32-bit PowerPC, broken in r328537.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@328650 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/powerpc/aim/mp_cpudep.c
@@ -85,15 +85,20 @@ cpudep_ap_early_bootstrap(void)
 		break;
 	case IBMPOWER8:
 	case IBMPOWER8E:
+#ifdef __powerpc64__
 		if (mfmsr() & PSL_HV) {
 			isync();
-			/* Direct interrupts to SRR instead of HSRR and reset LPCR otherwise */
+			/*
+			 * Direct interrupts to SRR instead of HSRR and
+			 * reset LPCR otherwise
+			 */
 			mtspr(SPR_LPID, 0);
 			isync();
 
 			mtspr(SPR_LPCR, LPCR_LPES);
 			isync();
 		}
+#endif
 		break;
 	}
 