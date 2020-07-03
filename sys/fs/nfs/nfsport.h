bdrewery/freebsd

commit ffcf1c288025b94691b55f51b436ea3914ab1585

Add support for ext_pgs mbufs to nfsm_build().
This is the first of a series of commits that add support to the NFS client
and server for building RPC messages in ext_pgs mbufs with anonymous pages.
This is useful so that the entire mbuf list does not need to be
copied before calling sosend() when NFS over TLS is enabled.

Since ND_EXTPG is never set yet, there is no semantic change at this time.


git-svn-id: svn+ssh://svn.freebsd.org/base/head@362903 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/fs/nfs/nfsport.h
@@ -109,8 +109,9 @@
 #include <ufs/ufs/ufsmount.h>
 #include <vm/uma.h>
 #include <vm/vm.h>
-#include <vm/vm_object.h>
 #include <vm/vm_extern.h>
+#include <vm/vm_object.h>
+#include <vm/vm_param.h>
 #include <nfs/nfssvc.h>
 #include "opt_nfs.h"
 #include "opt_ufs.h"

sys/fs/nfs/nfsm_subs.h
@@ -64,14 +64,27 @@ nfsm_build(struct nfsrv_descript *nd, int siz)
 	void *retp;
 	struct mbuf *mb2;
 
-	if (siz > M_TRAILINGSPACE(nd->nd_mb)) {
+	if ((nd->nd_flag & ND_EXTPG) == 0 &&
+	    siz > M_TRAILINGSPACE(nd->nd_mb)) {
 		NFSMCLGET(mb2, M_NOWAIT);
 		if (siz > MLEN)
 			panic("build > MLEN");
 		mb2->m_len = 0;
-		nd->nd_bpos = mtod(mb2, caddr_t);
+		nd->nd_bpos = mtod(mb2, char *);
 		nd->nd_mb->m_next = mb2;
 		nd->nd_mb = mb2;
+	} else if ((nd->nd_flag & ND_EXTPG) != 0) {
+		if (siz > nd->nd_bextpgsiz) {
+			mb2 = mb_alloc_ext_plus_pages(PAGE_SIZE, M_WAITOK);
+			nd->nd_bpos = (char *)(void *)
+			    PHYS_TO_DMAP(mb2->m_epg_pa[0]);
+			nd->nd_bextpg = 0;
+			nd->nd_bextpgsiz = PAGE_SIZE - siz;
+			nd->nd_mb->m_next = mb2;
+			nd->nd_mb = mb2;
+		} else
+			nd->nd_bextpgsiz -= siz;
+		nd->nd_mb->m_epg_last_len += siz;
 	}
 	retp = (void *)(nd->nd_bpos);
 	nd->nd_mb->m_len += siz;

commit 01777b52cc76c8f9aad40ea62bd09b9f2b8f8c3b

Fix build breakage caused by r362903. Only pmap.h is needed now, but vm_page.h and vm_pageout.h is needed later, so put them in now.
Pointy hat goes on me.


git-svn-id: svn+ssh://svn.freebsd.org/base/head@362906 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/fs/nfs/nfsport.h
@@ -109,8 +109,11 @@
 #include <ufs/ufs/ufsmount.h>
 #include <vm/uma.h>
 #include <vm/vm.h>
+#include <vm/pmap.h>
 #include <vm/vm_extern.h>
 #include <vm/vm_object.h>
+#include <vm/vm_page.h>
+#include <vm/vm_pageout.h>
 #include <vm/vm_param.h>
 #include <nfs/nfssvc.h>
 #include "opt_nfs.h"