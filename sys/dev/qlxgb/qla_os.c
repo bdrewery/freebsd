bdrewery/freebsd

commit 72eafe6a2ebddb933db7771241c5018207e827a4

No need to set if_mtu since it automatically updated by ether_ifattach(). Use  if_initbaudrate() to set baudrate. Add IFCAP_LINKSTATE to if_capabilities.
Submitted by:	David C Somayajulu <davidcs@freebsd.org>
Approved by:	George Neville-Neil <gnn@freebsd.org>


git-svn-id: svn://svn.freebsd.org/base/head@250375 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/qlxgb/qla_os.c
@@ -669,8 +669,7 @@ qla_init_ifnet(device_t dev, qla_host_t *ha)
 
 	if_initname(ifp, device_get_name(dev), device_get_unit(dev));
 
-	ifp->if_mtu = ETHERMTU;
-	ifp->if_baudrate = (1 * 1000 * 1000 *1000);
+	if_initbaudrate(ifp, IF_Gbps(10));
 	ifp->if_init = qla_init;
 	ifp->if_softc = ha;
 	ifp->if_flags = IFF_BROADCAST | IFF_SIMPLEX | IFF_MULTICAST;
@@ -690,6 +689,7 @@ qla_init_ifnet(device_t dev, qla_host_t *ha)
 				IFCAP_JUMBO_MTU;
 
 	ifp->if_capabilities |= IFCAP_VLAN_HWTAGGING | IFCAP_VLAN_MTU;
+	ifp->if_capabilities |= IFCAP_LINKSTATE;
 
 #if defined(__FreeBSD_version) && (__FreeBSD_version < 900002)
 	ifp->if_timer = 0;

commit 46dfa4b1d7b1927ff4390de3ac4c16372fd6e900

qlxgb: Initialize if_mtu before setting max_frame_size.
Previously we were relying on ether_ifattach() to set if_mtu, but
max_frame_size is initialized earlier.  This fixes a regression
introduced by r250375.

PR:		249050
Submitted by:	Christian Vallières <novacrash_@hotmail.com>
MFC after:	3 days


git-svn-id: svn+ssh://svn.freebsd.org/base/head@365060 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/qlxgb/qla_os.c
@@ -671,6 +671,7 @@ qla_init_ifnet(device_t dev, qla_host_t *ha)
 
 	if_initname(ifp, device_get_name(dev), device_get_unit(dev));
 
+	ifp->if_mtu = ETHERMTU;
 	ifp->if_baudrate = IF_Gbps(10);
 	ifp->if_init = qla_init;
 	ifp->if_softc = ha;